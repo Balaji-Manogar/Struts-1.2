d4054079a6206772646377e030955981d0839f58
Balaji <rajanbalaji2000@yahoo.co.in>
2014-07-14 16:25:17 +0530

    Simple ActionForward test

---
+++ test.struts1.3/src/main/java/org/baali/struts/logon/ActionForwardDemo.java
@@ -0,0 +1,20 @@
+package org.baali.struts.logon;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class ActionForwardDemo extends Action
+{
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest req, HttpServletResponse res)
+	{
+		ActionForward af = mapping.findForward("forward");
+		String path = af.getPath().toString() + "/forward.jsp?name=Balaji";
+		return new ActionForward(path);
+	}
+
+}
--- test.struts1.3/src/main/webapp/index.jsp
+++ test.struts1.3/src/main/webapp/index.jsp
@@ -1,8 +1,10 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="html" uri="http://jakarta.apache.org/struts/tags-html"  %>
 <html>
 <body>
 <c:set var="hello" value="Hello Maven" />
 
 <h2>${hello}..</h2>
+<html:link action="action-forward">Action forward demo</html:link>
 </body>
 </html>
